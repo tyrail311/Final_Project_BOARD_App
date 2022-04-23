be86edd52ebcfad7d613a818bb1af4100e2012fd
tpnadeau <tyrail311@gmail.com>
2022-04-23 12:31:32 -0400

    initial commit

---
+++ app/src/main/java/com/example/final_project_board_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.final_project_board_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
